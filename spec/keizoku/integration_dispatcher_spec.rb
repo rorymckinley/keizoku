832bfbeaf3441866fd8f420132de4f5f0cb3e477
Sheldon Hearn <sheldonh@starjuice.net>
2012-07-29 15:29:39 +0200

    Prevent keizoku-integration from being run

--- spec/keizoku/integration_dispatcher_spec.rb
+++ spec/keizoku/integration_dispatcher_spec.rb
@@ -22,11 +22,13 @@ describe Keizoku::IntegrationDispatcher do
   end
 
   let(:request) { {:workbench => 'workbench_sprint66'} }
+  let(:integration) { double(Keizoku::Integration, :request => request).as_null_object }
+  let(:integration_factory) { ->(r) { integration } }
 
   describe "#start_integrating(request)" do
 
     it "adds an integration for the request to the pool" do
-      @dispatcher = Keizoku::IntegrationDispatcher.new(1)
+      @dispatcher = Keizoku::IntegrationDispatcher.new(1, integration_factory)
       @dispatcher.start_integrating(request)
       @dispatcher.should be_busy_with(request)
     end
@@ -72,7 +74,7 @@ describe Keizoku::IntegrationDispatcher do
 
   describe "#empty?" do
     before(:each) do
-      @dispatcher = Keizoku::IntegrationDispatcher.new(1, ->(r) { double.as_null_object })
+      @dispatcher = Keizoku::IntegrationDispatcher.new(1, integration_factory)
     end
 
     it "is false when integrations are in progress" do
@@ -98,13 +100,13 @@ describe Keizoku::IntegrationDispatcher do
   describe "#full?" do
 
     it "is false when there is capacity for at least one more integration" do
-      @dispatcher = Keizoku::IntegrationDispatcher.new(2)
+      @dispatcher = Keizoku::IntegrationDispatcher.new(2, integration_factory)
       @dispatcher.start_integrating(request)
       @dispatcher.should_not be_full
     end
 
     it "is true when there is no capacity for another integration" do
-      @dispatcher = Keizoku::IntegrationDispatcher.new(1)
+      @dispatcher = Keizoku::IntegrationDispatcher.new(1, integration_factory)
       @dispatcher.start_integrating(request)
       @dispatcher.should be_full
     end
@@ -114,7 +116,8 @@ describe Keizoku::IntegrationDispatcher do
   describe "#busy_filter" do
 
     before(:each) do
-      @dispatcher = Keizoku::IntegrationDispatcher.new(1).tap { |o| o.start_integrating(request) }
+      @dispatcher = Keizoku::IntegrationDispatcher.new(1, integration_factory)
+      @dispatcher.start_integrating(request)
     end
     let(:filter) { @dispatcher.busy_filter }
 
