3f2600caf8706839b4fdd1b5995ff45b5d59c59c
Sheldon Hearn <sheldonh@starjuice.net>
2012-07-30 16:12:31 +0200

    Tidy up specs
    These specs got into a it of a mess as we plummetd down the fork/exec
    rabbit hole.

--- spec/keizoku/integration_spec.rb
+++ spec/keizoku/integration_spec.rb
@@ -3,7 +3,8 @@ require 'keizoku/integration'
 
 describe Keizoku::Integration do
   let(:request) { {:keizoku_is_awesome => "junk", :keizoku_is_testing => "stuff"} }
-  let(:integration) { Keizoku::Integration.build(request, "keizoku-validate-rake-spec") }
+  let(:integration_script) { File.join(File.dirname(__FILE__), '..', 'support', 'fake-keizoku-integration') }
+  let(:integration) { Keizoku::Integration.build(request, '/bin/true', integration_script) }
 
   describe ".build(request)" do
     it "returns an integration for the request" do
@@ -12,99 +13,57 @@ describe Keizoku::Integration do
   end
 
   describe "#integrate" do
+    before(:each) { integration.integrate }
+
     it "defines a request-specific environment for the integration script" do
-      helper = File.join(File.dirname(__FILE__), '..', 'support', 'fake-keizoku-integration')
-      @integration = Keizoku::Integration.build(request, "/bin/true", helper)
-      @integration.integrate
-      @integration.log.should include("KEIZOKU_IS_AWESOME=junk")
-      @integration.log.should include("KEIZOKU_IS_TESTING=stuff")
+      integration.log.should include("KEIZOKU_IS_AWESOME=junk")
+      integration.log.should include("KEIZOKU_IS_TESTING=stuff")
     end
 
     it "does not touch the process environment" do
-      helper = File.join(File.dirname(__FILE__), '..', 'support', 'fake-keizoku-integration')
-      @integration = Keizoku::Integration.build(request, "/bin/true", helper)
-      @integration.integrate
       request.each { |key, value| ENV.should_not include(key.to_s.upcase) }
     end
 
     it "it includes the process environment in the environment it defines for the integration script" do
-      helper = File.join(File.dirname(__FILE__), '..', 'support', 'fake-keizoku-integration')
-      @integration = Keizoku::Integration.build(request, "/bin/true", helper)
-      @integration.integrate
-      @integration.log.should include("PATH=#{ENV['PATH']}")
+      integration.log.should include("PATH=#{ENV['PATH']}")
+    end
+  end
+
+  describe "#log" do
+    it "makes a log of the integration attempt available" do
+      integration = Keizoku::Integration.build(request, "/bin/echo Wonderful success", integration_script)
+      integration.integrate
+      integration.log.should match(/Wonderful success/)
     end
 
     it "logs an error if the integration script is not executable" do
-      helper = File.join(File.dirname(__FILE__), '..', 'support', 'does_not_exist')
-      @integration = Keizoku::Integration.build(request, "/bin/true", helper)
-      expect { @integration.integrate }.to_not raise_error
-      @integration.log.should include('No such file or directory')
-    end
-
-    # TODO this must go when integration tests prove system() is working
-    it "it works with an actual integration helper and real fork, with successful integration" do
-      helper = File.join(File.dirname(__FILE__), '..', 'support', 'fake-keizoku-integration')
-      integrations = [1, 2].collect { Keizoku::Integration.build(request, "/bin/true", helper) }
-      threads = integrations.collect { |i| Thread.fork { i.integrate } }
-      tempus_fugit
-      threads.each { |t| t.join }
-      integrations.each do |i|
-        i.should be_completed
-        i.should be_successful
-      end
-    end
-
-    # TODO this must go when integration tests prove system() is working
-    it "it works with an actual integration helper and real fork, with failed integration" do
-      helper = File.join(File.dirname(__FILE__), '..', 'support', 'fake-keizoku-integration')
-      integrations = [1, 2].collect { Keizoku::Integration.build(request, "/bin/false", helper) }
-      threads = integrations.collect { |i| Thread.fork { i.integrate } }
-      tempus_fugit
-      threads.each { |t| t.join }
-      integrations.each do |i|
-        i.should be_completed
-        i.should_not be_successful
-      end
-    end
-
-    it "makes a log of the integration attempt avaialble" do
-      helper = File.join(File.dirname(__FILE__), '..', 'support', 'fake-keizoku-integration')
-      @integration = Keizoku::Integration.build(request, "/bin/echo Wonderful success", helper)
-      thr = Thread.fork { @integration.integrate }
-      tempus_fugit
-      thr.join
-      @integration.log.should match(/Wonderful success/)
+      integration = Keizoku::Integration.build(request, "/bin/true", 'nonexistent-integration-helper')
+      expect { integration.integrate }.to_not raise_error
+      integration.log.should include('command not found')
     end
   end
 
   describe "#completed?" do
     it "is false if not completed" do
-      helper = File.join(File.dirname(__FILE__), '..', 'support', 'fake-keizoku-integration')
-      @integration = Keizoku::Integration.build(request, "/bin/true", helper)
-      @integration.should_not be_completed
+      integration.should_not be_completed
     end
 
     it "is true if completed (regardless of integration outcome)" do
-      helper = File.join(File.dirname(__FILE__), '..', 'support', 'fake-keizoku-integration')
-      @integration = Keizoku::Integration.build(request, "/bin/true", helper)
-      @integration.integrate
-      @integration.should be_completed
+      integration.integrate
+      integration.should be_completed
     end
   end
 
   describe "#successful?" do
     it "is true when the integration script was successful" do
-      helper = File.join(File.dirname(__FILE__), '..', 'support', 'fake-keizoku-integration')
-      @integration = Keizoku::Integration.build(request, "/bin/true", helper)
-      @integration.integrate
-      @integration.should be_successful
+      integration.integrate
+      integration.should be_successful
     end
 
     it "is false when the integration script was not successful" do
-      helper = File.join(File.dirname(__FILE__), '..', 'support', 'fake-keizoku-integration')
-      @integration = Keizoku::Integration.build(request, "/bin/false", helper)
-      @integration.integrate
-      @integration.should_not be_successful
+      integration = Keizoku::Integration.build(request, "/bin/false", integration_script)
+      integration.integrate
+      integration.should_not be_successful
     end
   end
 end
