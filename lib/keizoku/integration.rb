8d2619582129d8f725e59a091e6711450b2b4de2
Sheldon Hearn <sheldonh@starjuice.net>
2012-07-30 12:04:50 +0200

    Factor integration IPC into SubProcess class

--- lib/keizoku/integration.rb
+++ lib/keizoku/integration.rb
@@ -1,8 +1,8 @@
-require 'io/wait'
+require 'keizoku/sub_process'
 
 module Keizoku
   class Integration
-    attr_reader :request
+    attr_reader :log, :request
 
     def self.build(request, validator_path="keizoku-validate-rake-spec", integration_helper = "keizoku-integrate")
       new(request, validator_path, integration_helper)
@@ -16,57 +16,15 @@ module Keizoku
     private :initialize
 
     def integrate
-      log_r, log_w = IO.pipe
-      exit_r, exit_w = IO.pipe
-      pid = fork do
-        log_r.close
-        exit_r.close
-        ENV.replace(environment)
-        if File.executable?(@integration_helper)
-          log = `#{@integration_helper} 2>&1`
-          exit_w.puts $?.to_i
-          log_w.write log
-        end
-        exit_w.close
-        log_w.close
-      end
-      log_w.close
-      exit_w.close
-      @log = ''
-      while Process.wait(pid, Process::WNOHANG)
-        @successful = exit_r.gets.chomp == '0' if exit_r.ready?
-        @log += log_r.read if log_r.ready?
-        sleep 0.001
-      end
-      unless exit_r.eof?
-        @successful = exit_r.gets.chomp == '0'
-      end
-      @log += log_r.read unless log_r.eof?
-      exit_r.close
-      log_r.close
-      #@successful = system(environment, @integration_helper)
-      @completed = true
-      raise RuntimeError.new("Could not execute #{@integration_helper}") if @successful.nil?
-    end
-
-    def log
-      @log
+      @process = Keizoku::SubProcess.new(environment, @integration_helper)
+      @process.execute
+      harvest_process_outcome
     end
 
     def environment
-      merge process_environment, integration_environment, request_environment
+      integration_environment.merge(request_environment)
     end
     private :environment
-
-    def merge(*args)
-      args.inject({}) { |m, environment| m.merge! environment }
-    end
-    private :merge
-
-    def process_environment
-      ENV.to_hash
-    end
-    private :process_environment
 
     def integration_environment
       {"VALIDATOR" => @validator_path}
@@ -80,6 +38,13 @@ module Keizoku
       end
     end
     private :request_environment
+
+    def harvest_process_outcome
+      @log = @process.log
+      @successful = @process.successful?
+      @completed = true
+    end
+    private :harvest_process_outcome
 
     def completed?
       @completed
---
+++ lib/keizoku/sub_process.rb
@@ -0,0 +1,99 @@
+require 'io/wait'
+
+module Keizoku
+
+  class SubProcess
+
+    def initialize(environment, command)
+      @environment = environment
+      @command = command
+    end
+
+    def execute
+      open_ipc_pipes
+      @child_pid = fork do
+        child_process_handler
+      end
+      parent_process_handler
+    end
+
+    def log
+      @log
+    end
+
+    def successful?
+      @successful
+    end
+
+    def completed?
+      @completed
+    end
+
+    private
+
+    def open_ipc_pipes
+      @log_r, @log_w = IO.pipe
+      @exit_r, @exit_w = IO.pipe
+    end
+
+    def child_process_handler
+      close_ipc_readers
+      try_execute_child_process
+      close_ipc_writers
+    end
+
+    def try_execute_child_process
+      if File.executable?(@command)
+        execute_child_process
+      end
+    end
+
+    def execute_child_process
+      ENV.replace(process_environment)
+      @log_w.write `#{@command} 2>&1`
+      @exit_w.puts $?.to_i
+    end
+
+    def parent_process_handler
+      close_ipc_writers
+      harvest_child_process_outcome
+      close_ipc_readers
+      raise RuntimeError.new("Could not execute #{@command}") if @successful.nil?
+    end
+
+    def close_ipc_readers
+      @log_r.close
+      @exit_r.close
+    end
+
+    def close_ipc_writers
+      @log_w.close
+      @exit_w.close
+    end
+
+    def harvest_child_process_outcome
+      @log = ''
+      while Process.wait(@child_pid, Process::WNOHANG)
+        read_child_success if @exit_r.ready?
+        read_child_log if @log_r.ready?
+        sleep 0.001
+      end
+      read_child_success unless @exit_r.eof?
+      read_child_log unless @log_r.eof?
+    end
+
+    def read_child_success
+      @successful = @exit_r.gets.chomp == '0'
+    end
+
+    def read_child_log
+      @log += @log_r.read
+    end
+
+    def process_environment
+      ENV.to_hash.merge(@environment)
+    end
+
+  end
+
+end
